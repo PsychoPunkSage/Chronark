23522b3311504776bea6631c480a8e50b1fa7960
PsychoPunkSage <abhinav.prakash319@gmail.com>
2024-05-28 14:00:45 +0530

    fix::> improved the impl

--- src/offer_banner/ads.py
+++ src/offer_banner/ads.py
@@ -1,17 +1,18 @@
-import os.path
-import csv
 import json
-from flask import Flask
-from helper import redis
-from flask import request
+import random
+from helper import hredis
 
-dataPath = "src/offer_banner/ads.json"
+def get_offer_banner(banner_list):
+    if not banner_list:
+        return None
+    random_banner = random.choice(banner_list)
+    return random_banner
 
 class Ad:
-    def __init__(self, adID="", alt="", image_path="", category="", date="", time="") -> None:
+    def __init__(self, adID="", alt="", url="", category="", date="", time="") -> None:
         self.adID = adID
         self.alt = alt
-        self.image_path = image_path
+        self.url = url
         self.category = category
         self.date = date
         self.time = time
@@ -19,39 +20,34 @@ class Ad:
         return self.date + " || " + self.time
 
 def getAds():
-    # if os.path.isfile(dataPath):
-    #     with open(dataPath, newline='') as adFile:
-    #         data = adFile.read()
-    #         ads = json.loads(data)
-    #         return ads
-    # else:
-    #     print("Error loading")
-    #     return {}
     ads = {}
-    adIDs = redis.redis_command(redis.redis_command.scan_iter, "*")
+    adIDs = hredis.redis_command(hredis.r_client.scan_iter, "*")
     for adID in adIDs:
         ad = getAd(adID)
-        ads[ad["adID"]] = ad
+        if "adID" in ad:
+            ads[ad["adID"]] = ad
+        else:
+            print(f"Invalid ad data for adID {adID}: {ad}")
     return ads
 
 def getAd(adID):
-    # ads = getAds()
-    # if adID in ads:
-    #     return ads[adID]
-    # else:
-    #     return {}
-    ad = redis.redis_command(redis.redis_master.get, adID)
+    ad = hredis.redis_command(hredis.r_client.get, adID)
     if ad is None:
         return {}
     else:
-        return json.loads(str(ad, 'utf8'))
+        try:
+            return json.loads(ad.decode('utf-8'))
+        except json.JSONDecodeError as e:
+            print(f"JSONDecodeError: {e}")
+            print(f"Raw ad data: {ad}")
+            return {}
 
 def updateAd(ad):
-    redis.redis_command(redis.redis_master.set, ad.adID, json.dumps(ad.__dict__))
+    if isinstance(ad, Ad):
+        hredis.redis_command(hredis.r_client.set, ad.adID, json.dumps(ad.__dict__))
+    else:
+        raise TypeError("Expected an instance of Ad class")
 
 def updateAds(ads):
-    # with open(dataPath, 'w', newline='') as adFile:
-    #     adJson = json.dumps(adDict)
-    #     adFile.write(adJson)
     for ad in ads:
         updateAd(ad)
--- src/offer_banner/offerBanner.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-import redis
-
-def get_offer_banner(banner_list):
-    if not banner_list:
-        return None
-    random_banner = random.choice(banner_list)
-    return random_banner
