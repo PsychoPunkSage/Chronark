6ce585dd67b820bbb876574dacf4dacaa89f7084
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-05 10:46:57 +0530

    fix::> Changed impl to display a dummy index page for info

--- src/contact/app.py
+++ src/contact/app.py
@@ -8,14 +8,9 @@ name:
 employee: (yes/no)
 customer: (yes/no)
 email:
-mobile number:
+mobile:
 address:
 '''
-# SELF_PORT = 5003
-# MONGO_DB_HOST = "localhost"
-# MONGO_DB_PORT = "27017"
-# MONGO_DB_USERNAME = "mongocontact"
-# MONGO_DB_PASSWORD = "contact"
 SELF_PORT = os.environ.get('SELF_PORT')
 MONGO_DB_HOST = os.environ.get('MONGO_DB_HOST')
 MONGO_DB_PORT = os.environ.get('MONGO_DB_PORT')
@@ -31,43 +26,51 @@ print("db_client::> ", db_client)
 data = db_client.contact
 storage = data.storage
 
+class Client:
+    def __init__(self, id="", name="", employee="", customer="", email="", mobile="", address="") -> None:
+        self.id = id
+        self.name = name
+        self.employee = employee
+        self.customer = customer
+        self.email = email
+        self.mobile = mobile
+        self.address = address
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', m_client=db_client, MONGO_HOST=MONGO_DB_HOST, MONGO_PORT=MONGO_DB_PORT, MONGO_PASSWORD=MONGO_DB_PASSWORD, MONGO_USERNAME=MONGO_DB_USERNAME)
 
-
-# connecting to db
-@app.route('/client', methods=['POST'])
-def add_client():
-    client_data = request.json
-    status = storage.insert_one(client_data)
-    return jsonify({"status": str(status.inserted_id)}), 201
-
-@app.route('/client/<client_id>', methods=['GET'])
-def get_client(client_id):
-    client = storage.find_one({"id": client_id})
-    if client:
-        return jsonify(client), 200
-    else:
-        return jsonify({"error": "Client not found"}), 404
-
-@app.route('/clients', methods=['GET'])
-def get_all_clients():
+@app.route('/getClients', methods=['GET'])
+def getClients():
     client_data = storage.find()
     clients = []
     for client in client_data:
-        client['_id'] = str(client['_id'])  # Convert ObjectId to string
-        clients.append(client)
-    return jsonify(clients), 200
+        client_dict = {key: value for key, value in client.items() if key != '_id'}
+        clients.append(client_dict)
+    return jsonify(clients)
 
-@app.route('/client/<client_id>', methods=['PUT'])
-def update_client(client_id):
-    updated_data = request.json
-    result = storage.update_one({"id": client_id}, {"set": updated_data})
-    if result.matched_count:
-        return jsonify({"status": "Client updated"}), 200
+@app.route('/getClient/<string:id>', methods=['GET'])
+def getClient(id):
+    client = storage.find_one({"id": id})
+    if client is None:
+        return {}, 404
     else:
-        return jsonify({"error": "Client not found"}), 404
+        return client
+
+@app.route('/updateClient', methods=['POST'])
+def updateClient():
+    jsonData = request.json
+    client = Client(**jsonData)
+    storage.update_one({"id": client.id}, {"$set": client.__dict__}, upsert=True)
+    return "Success", 200
+
+@app.route('/updateClients', methods=['POST'])
+def updateClients():
+    jsonData = request.json
+    for client_data in jsonData:
+        client = Client(**client_data)
+        storage.update_one({"id": client.id}, {"$set": client.__dict__}, upsert=True)
+    return "Success", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=SELF_PORT, debug=True)
