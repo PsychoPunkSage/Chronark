d1bb5461dede13a10d6c3198185d4a85210c08f5
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-08 15:13:24 +0530

    fix::> added calls and method of FAQs and Contact details

--- src/contact/app.py
+++ src/contact/app.py
@@ -22,9 +22,14 @@ app = Flask(__name__)
 
 # Database connection
 db_client = MongoClient(username=MONGO_DB_USERNAME, password=MONGO_DB_PASSWORD, host=MONGO_DB_HOST, port=int(MONGO_DB_PORT))
-print("db_client::> ", db_client)
+# User data
+# print("db_client::> ", db_client)
 data = db_client.contact
 storage = data.storage
+
+# Contact No.
+contacts_collection = data.contacts
+
 
 class Client:
     def __init__(self, id="", name="", employee="", customer="", email="", mobile="", address="") -> None:
@@ -36,9 +41,53 @@ class Client:
         self.mobile = mobile
         self.address = address
 
+class ContactNumber:
+    def __init__(self, region_id="", number="", region="", email=""):
+        self.region_id = region_id
+        self.number = number
+        self.region = region
+        self.email = email
+
+class Faq:
+    def __init__(self, question_id="", category="", question="", answer=""):
+        self.question_id = question_id
+        self.category = category
+        self.question = question
+        self.answer = answer
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', m_client=db_client, MONGO_HOST=MONGO_DB_HOST, MONGO_PORT=MONGO_DB_PORT, MONGO_PASSWORD=MONGO_DB_PASSWORD, MONGO_USERNAME=MONGO_DB_USERNAME)
+
+# ===================================================================================================================================================================================== #
+
+@app.route('/getContacts', methods=['GET'])
+def getContacts():
+    contact_data = contacts_collection.find()
+    contacts = []
+    for contact in contact_data:
+        contact_dict = {key: value for key, value in contact.items() if key != '_id'}
+        contacts.append(contact_dict)
+    return jsonify(contacts)
+
+@app.route('/updateContacts', methods=['POST'])
+def updateContacts():
+    jsonData = request.json
+    region_id = jsonData.get('region_id')
+    existing_contact = contacts_collection.find_one({"region_id": region_id})
+
+    if existing_contact:
+        contacts_collection.update_one({"region_id": region_id}, {"$set": jsonData})
+    else:
+        contacts_collection.insert_one(jsonData)
+    return "Success", 200
+
+# ===================================================================================================================================================================================== #
+
+@app.route('/clearContacts', methods=['POST'])
+def clearContacts():
+    contacts_collection.delete_many({})
+    return "All data cleared from contacts collection", 200
 
 @app.route('/getClients', methods=['GET'])
 def getClients():
@@ -48,6 +97,8 @@ def getClients():
         client_dict = {key: value for key, value in client.items() if key != '_id'}
         clients.append(client_dict)
     return jsonify(clients)
+
+# ===================================================================================================================================================================================== #
 
 @app.route('/getClient/<string:id>', methods=['GET'])
 def getClient(id):
@@ -60,9 +111,16 @@ def getClient(id):
 @app.route('/updateClient', methods=['POST'])
 def updateClient():
     jsonData = request.json
-    client = Client(**jsonData)
-    storage.update_one({"id": client.id}, {"$set": client.__dict__}, upsert=True)
+    client_id = jsonData.get('region_id')
+    existing_contact = contacts_collection.find_one({"id": client_id})
+
+    if existing_contact:
+        storage.update_one({"id": client_id}, {"$set": jsonData})
+    else:
+        storage.insert_one(jsonData)
     return "Success", 200
+
+# ===================================================================================================================================================================================== #
 
 @app.route('/updateClients', methods=['POST'])
 def updateClients():
