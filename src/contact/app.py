1739416745d073704d92c9c3848556e6e5e47a7b
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-08 19:32:52 +0530

    fix::> cantact basic completed

--- src/contact/app.py
+++ src/contact/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
@@ -30,6 +31,12 @@ storage = data.storage
 # Contact No.
 contacts_collection = data.contacts
 
+# FAQ
+faqs_collection = data.faq
+
+# Conversation
+conv_collection = data.conversation
+
 
 class Client:
     def __init__(self, id="", name="", employee="", customer="", email="", mobile="", address="") -> None:
@@ -55,9 +62,23 @@ class Faq:
         self.question = question
         self.answer = answer
 
+class Conversation:
+    def __init__(self, conversation_id="", name="", email="", message="", date="", time=""):
+        self.conversation_id = conversation_id
+        self.name = name
+        self.email = email
+        self.question = message
+        self.date = date
+        self.time = time
+
+# ===================================================================================================================================================================================== #
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', m_client=db_client, MONGO_HOST=MONGO_DB_HOST, MONGO_PORT=MONGO_DB_PORT, MONGO_PASSWORD=MONGO_DB_PASSWORD, MONGO_USERNAME=MONGO_DB_USERNAME)
+    conv_data = getConvs().response
+    # conv_data_json = [json.loads(item.decode('utf-8')) for item in conv_data]
+    print(conv_data)
+    return render_template('index.html', conv_data=conv_data, m_client=db_client, MONGO_HOST=MONGO_DB_HOST, MONGO_PORT=MONGO_DB_PORT, MONGO_PASSWORD=MONGO_DB_PASSWORD, MONGO_USERNAME=MONGO_DB_USERNAME)
 
 # ===================================================================================================================================================================================== #
 
@@ -84,10 +105,52 @@ def updateContacts():
 
 # ===================================================================================================================================================================================== #
 
+@app.route('/getFaqs', methods=['GET'])
+def getFaqs():
+    faq_data = faqs_collection.find()
+    faqs = []
+    for faq in faq_data:
+        faq_dict = {key: value for key, value in faq.items() if key != '_id'}
+        faqs.append(faq_dict)
+    return jsonify(faqs)
+
+@app.route('/updateFaqs', methods=['POST'])
+def updateFaqs():
+    jsonData = request.json
+    question_id = jsonData.get('question_id')
+    existing_faq = faqs_collection.find_one({"question_id": question_id})
+
+    if existing_faq:
+        faqs_collection.update_one({"question_id": question_id}, {"$set": jsonData})
+    else:
+        faqs_collection.insert_one(jsonData)
+    return "Success", 200
+
+# ===================================================================================================================================================================================== #
+
+@app.route('/getConvs', methods=['GET'])
+def getConvs():
+    conv_data = conv_collection.find()
+    convs = []
+    for conv in conv_data:
+        conv_dict = {key: value for key, value in conv.items() if key != '_id'}
+        convs.append(conv_dict)
+    return jsonify(convs)
+
+@app.route('/updateConvs', methods=['POST'])
+def updateConvs():
+    jsonData = request.json
+    conv_collection.insert_one(jsonData)
+    return "Success", 200
+
+# ===================================================================================================================================================================================== #
+
 @app.route('/clearContacts', methods=['POST'])
 def clearContacts():
     contacts_collection.delete_many({})
     return "All data cleared from contacts collection", 200
+
+# ===================================================================================================================================================================================== #
 
 @app.route('/getClients', methods=['GET'])
 def getClients():
@@ -98,7 +161,6 @@ def getClients():
         clients.append(client_dict)
     return jsonify(clients)
 
-# ===================================================================================================================================================================================== #
 
 @app.route('/getClient/<string:id>', methods=['GET'])
 def getClient(id):
@@ -121,14 +183,6 @@ def updateClient():
     return "Success", 200
 
 # ===================================================================================================================================================================================== #
-
-@app.route('/updateClients', methods=['POST'])
-def updateClients():
-    jsonData = request.json
-    for client_data in jsonData:
-        client = Client(**client_data)
-        storage.update_one({"id": client.id}, {"$set": client.__dict__}, upsert=True)
-    return "Success", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=SELF_PORT, debug=True)
