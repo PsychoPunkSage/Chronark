39d9417c39b4868158c223048f76715e7f0e3f85
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-24 11:01:33 +0530

    fix::> Improved the impl added env var

--- src/authentication/app.py
+++ src/authentication/app.py
@@ -6,13 +6,25 @@ import datetime
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
+from helper import redis_command
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
-mongo_client = MongoClient('mongodb://mongo:27017/')
-db = mongo_client['auth_db']
-users_collection = db['users']
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_super_complex_secret_key_here')
+
+SELF_PORT = os.environ.get('SELF_PORT')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+MONGO_DB_HOST = os.environ.get('MONGO_DB_HOST')
+MONGO_DB_PORT = os.environ.get('MONGO_DB_PORT')
+MONGO_DB_USERNAME = os.environ.get('MONGO_DB_USERNAME')
+MONGO_DB_PASSWORD = os.environ.get('MONGO_DB_PASSWORD')
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
+mongo_client = MongoClient(username=MONGO_DB_USERNAME, password=MONGO_DB_PASSWORD, host=MONGO_DB_HOST, port=int(MONGO_DB_PORT))
+data = mongo_client.auth_db
+users_collection = data.users
 
 def token_required(f):
     def decorator(*args, **kwargs):
@@ -22,9 +34,11 @@ def token_required(f):
 
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            if redis_client.get(token):
+            if redis_command(redis_client.get, token):
                 return jsonify({'message': 'Token is blacklisted'}), 403
-        except:
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 403
+        except jwt.InvalidTokenError:
             return jsonify({'message': 'Token is invalid'}), 403
 
         return f(*args, **kwargs)
@@ -33,6 +47,9 @@ def token_required(f):
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
+    if users_collection.find_one({'username': data['username']}):
+        return jsonify({'message': 'Username already exists'}), 409
+
     hashed_password = generate_password_hash(data['password'], method='sha256')
     users_collection.insert_one({'username': data['username'], 'password': hashed_password})
     return jsonify({'message': 'Registered successfully'}), 201
@@ -41,17 +58,20 @@ def register():
 def login():
     data = request.get_json()
     user = users_collection.find_one({'username': data['username']})
-    if user and check_password_hash(user['password'], data['password']):
+    if not user:
+        return jsonify({'message': 'Username does not exist'}), 404
+
+    if check_password_hash(user['password'], data['password']):
         token = jwt.encode({'user': data['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         return jsonify({'token': token})
 
-    return jsonify({'message': 'Could not verify'}), 401
+    return jsonify({'message': 'Could not verify password'}), 401
 
 @app.route('/logout', methods=['POST'])
 @token_required
 def logout():
     token = request.headers['Authorization']
-    redis_client.set(token, 'blacklisted', ex=30*60)  # Blacklist for 30 minutes
+    redis_command(redis_client.set, token, 'blacklisted', ex=30*60)  # Blacklist for 30 minutes
     return jsonify({'message': 'Logged out successfully'})
 
 @app.route('/protected', methods=['GET'])
