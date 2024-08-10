1faf03480a6a56c7268ec99c030ff5ceb1c585bc
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-10 17:13:42 +0530

    fix::> added multiple paths

--- src/customerInfo/app.py
+++ src/customerInfo/app.py
@@ -39,25 +39,43 @@ class CustomerInfoTemplate:
         self.username = username
         self.password = password
         self.name = name
-        self.acc_balance = acc_balance
-        self.dmat_balance = dmat_balance
-        self.account_number = account_number
-        self.acc_balance = acc_balance
+        self.acc_balance = acc_balance # default = 5000
+        self.dmat_balance = dmat_balance # default = 0
+        self.account_number = account_number # generated
         self.email = email
         self.contact_no = contact_no
         self.address = address
-        self.customer_pic_url = customer_pic_url
+        self.customer_pic_url = customer_pic_url # No need to add
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template(
+        'index.html',
+        m_client=db_client , 
+        MONGO_DB_HOST=MONGO_DB_HOST, 
+        MONGO_DB_PORT=MONGO_DB_PORT, 
+        MONGO_DB_USERNAME=MONGO_DB_USERNAME, 
+        MONGO_DB_PASSWORD=MONGO_DB_PASSWORD
+    )
 
 # ===================================================================================================================================================================================== #
 
-@app.route("/getCustomerInfo", methods=["GET"])
-def getCustomerInfo():
-    customer_data = customer_collection.find()
-    infos = []
-    for info in customer_data:
-        info_dict = {key: value for key, value in info.items() if key != '_id'}
-        infos.append(info_dict)
-    return jsonify(infos)
+@app.route("/getCustomerInfo/<string:username>", methods=["GET"])
+def getCustomerInfo(username):
+    customer_data = customer_collection.find_one({"username": username}, {"_id": 0})
+    if customer_data is None:
+        return jsonify({"message": "No customer found"}), 404
+    else:
+        return jsonify(customer_data)
+
+@app.route("/getCustomerInfos", methods=["GET"])
+def getCustomerInfos():
+    customer_datas = customer_collection.find()
+    datas = []
+    for contact in customer_datas:
+        contact_dict = {key: value for key, value in contact.items() if key != '_id'}
+        datas.append(contact_dict)
+    return jsonify(datas)
 
 @app.route("/updateCustomerInfo", methods=["POST"])
 def updateCustomerInfo():
@@ -66,9 +84,22 @@ def updateCustomerInfo():
     existing_user = customer_collection.find_one({"username": username})
 
     if existing_user:
+        # Preserve the existing values for account_balance, dmat_balance, and account_number
+        jsondata['acc_balance'] = existing_user.get('acc_balance')
+        jsondata['dmat_balance'] = existing_user.get('dmat_balance')
+        jsondata['account_number'] = existing_user.get('account_number')
+
         customer_collection.update_one({"username": username}, {"$set": jsondata})
     else:
         customer_collection.insert_one(jsondata)
     return "Success", 200
 
 # ===================================================================================================================================================================================== #
+
+@app.route('/clearData', methods=['POST'])
+def clearContacts():
+    customer_collection.delete_many({})
+    return "All data cleared from contacts collection", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=SELF_PORT, debug=True)
