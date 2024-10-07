649d4d78c83b4d33c31eed1c1cda6c49bad3b697
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-08 00:42:15 +0530

    fix::> removed useless files

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ if [ $? -eq 0 ]; then
     echo "Python script executed successfully."
     echo ""
     echo "==================================="
-    echo "|| Visit: http://localhost:4001/ ||"
+    echo "|| Visit: http://localhost:80/ ||"
     echo "==================================="
 
     echo "Note: Visit ./src/api to get an idea of structure of API calls."
