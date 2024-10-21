dda7d68f96ee1fc44dea5b4f59df7f9de88f3ec8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-21 15:43:47 +0530

    fix::> Jaeger integrated

--- src/authentication/app.py
+++ src/authentication/app.py
@@ -10,6 +10,9 @@ from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
 from helper import redis_command
 
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_super_complex_secret_key_here')
 
@@ -25,6 +28,32 @@ AUTH_MONGO_DB_PASSWORD = os.environ.get('MONGO_DB_PASSWORD')
 CUSTOMER_INFO_SERVICE_HOST = os.environ.get('CUSTOMER_INFO_SERVICE_HOST')
 CUSTOMER_INFO_SERVICE_PORT = os.environ.get('CUSTOMER_INFO_SERVICE_PORT')
 CUSTOMER_INFO_SERVICE_URL = f'http://{CUSTOMER_INFO_SERVICE_HOST}:{CUSTOMER_INFO_SERVICE_PORT}'
+# Jaegar integration
+JAEGER_AGENT_HOST = os.environ.get('JAEGER_AGENT_HOST')
+JAEGER_AGENT_PORT = os.environ.get('JAEGER_AGENT_PORT')
+JAEGER_SERVICE_URL = 'http://' + JAEGER_AGENT_HOST + ':' + JAEGER_AGENT_PORT
+
+def init_tracer():
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'local_agent': {
+                'reporting_host': JAEGER_AGENT_HOST,
+                'reporting_port': JAEGER_AGENT_PORT,
+            },
+            'logging': True,
+        },
+        service_name='ms-authentication',
+    )
+    return config.initialize_tracer()
+
+tracer = init_tracer()
+tracing = FlaskTracing(tracer, True, app)
+
+# ================================================================================================ #
 
 redis_client = redis.Redis(host=AUTH_REDIS_HOST, port=AUTH_REDIS_PORT, password=AUTH_REDIS_PASSWORD)
 
@@ -51,31 +80,6 @@ print(f"Fetched document: {fetched_user}")
 delete_result = users_collection.delete_one({"username": "test_user"})
 print(f"Deleted document count: {delete_result.deleted_count}")
 
-# @app.route('/test_db', methods=['GET'])
-# def test_db_connection():
-#     try:
-#         # Insert a test document
-#         test_user = {"username": "test_user", "password": "test_password"}
-#         insert_result = users_collection.insert_one(test_user)
-#         print(f"Inserted document ID: {insert_result.inserted_id}")
-
-#         # Fetch the test document
-#         fetched_user = users_collection.find_one({"username": "test_user"})
-#         print(f"Fetched document: {fetched_user}")
-
-#         # Delete the test document
-#         delete_result = users_collection.delete_one({"username": "test_user"})
-#         print(f"Deleted document count: {delete_result.deleted_count}")
-
-#         return jsonify({
-#             "inserted_id": str(insert_result.inserted_id),
-#             "fetched_user": fetched_user,
-#             "deleted_count": delete_result.deleted_count
-#         })
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#         return jsonify({"error": str(e)}), 500
-
 def token_required(f):
     def decorated_function(*args, **kwargs):
         token = request.headers.get('Authorization')
@@ -95,86 +99,93 @@ def token_required(f):
     return decorated_function
 
 @app.route('/', methods=['GET'])
+@tracing.trace()
 def index():
-    return render_template(
-        'index.html',
-        r_client=redis_client,
-        AUTH_REDIS_HOST=AUTH_REDIS_HOST,
-        AUTH_REDIS_PORT=AUTH_REDIS_PORT,
-        AUTH_REDIS_PASSWORD=AUTH_REDIS_PASSWORD,
-        m_client=mongo_client,
-        MONGO_DB_HOST=AUTH_MONGO_DB_HOST,
-        MONGO_DB_PORT=AUTH_MONGO_DB_PORT,
-        MONGO_DB_USERNAME=AUTH_MONGO_DB_USERNAME,
-        MONGO_DB_PASSWORD=AUTH_MONGO_DB_PASSWORD
-    )
+    with tracer.start_active_span('/auth/') as scope:
+        return render_template(
+            'index.html',
+            r_client=redis_client,
+            AUTH_REDIS_HOST=AUTH_REDIS_HOST,
+            AUTH_REDIS_PORT=AUTH_REDIS_PORT,
+            AUTH_REDIS_PASSWORD=AUTH_REDIS_PASSWORD,
+            m_client=mongo_client,
+            MONGO_DB_HOST=AUTH_MONGO_DB_HOST,
+            MONGO_DB_PORT=AUTH_MONGO_DB_PORT,
+            MONGO_DB_USERNAME=AUTH_MONGO_DB_USERNAME,
+            MONGO_DB_PASSWORD=AUTH_MONGO_DB_PASSWORD
+        )
 
 @app.route('/register', methods=['POST'])
+@tracing.trace()
 def register():
-    jsonData = request.json
-    username = jsonData.get('username')
-    password = jsonData.get('password')
-    existing_user = users_collection.find_one({'username': username})
+    with tracer.start_active_span('/auth/register') as scope:
+        jsonData = request.json
+        username = jsonData.get('username')
+        password = jsonData.get('password')
+        existing_user = users_collection.find_one({'username': username})
 
-    loginData = {'username': username, 'password': password}
-    
-    if existing_user:
-        users_collection.update_one({'username': username}, {"$set": loginData})
-    else:
-        # New User
-        users_collection.insert_one(loginData)
-        customerData = makeCustomerInfo(jsonData=jsonData)
-        response = requests.post(f'{CUSTOMER_INFO_SERVICE_URL}/updateCustomerInfo', json = customerData)
-        if response.status_code != 200:
-            return jsonify({'message': 'Failed to Add customer'}), 500
-    return "Successful Regiatration", 200
+        loginData = {'username': username, 'password': password}
+
+        if existing_user:
+            users_collection.update_one({'username': username}, {"$set": loginData})
+        else:
+            # New User
+            users_collection.insert_one(loginData)
+            customerData = makeCustomerInfo(jsonData=jsonData)
+            response = requests.post(f'{CUSTOMER_INFO_SERVICE_URL}/updateCustomerInfo', json = customerData)
+            if response.status_code != 200:
+                return jsonify({'message': 'Failed to Add customer'}), 500
+        return "Successful Regiatration", 200
 
 
 @app.route('/login', methods=['POST'])
+@tracing.trace()
 def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-    user = users_collection.find_one({'username': username})
-    if not user:
-        return jsonify({'message': 'Username does not exist'}), 404
-    else:
-        print("user password", user["password"])
-        if user["password"]!=password:
-            return "UnAuthenticated Access", 303
+    with tracer.start_active_span('/auth/login') as scope:
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+        user = users_collection.find_one({'username': username})
+        if not user:
+            return jsonify({'message': 'Username does not exist'}), 404
         else:
-            payload = {
-                'user': username,
-                'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=30)
-            }
-            token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-            return jsonify({'Status': "Success", "Status Code": 200, "token": token, "username": username})
+            print("user password", user["password"])
+            if user["password"]!=password:
+                return "UnAuthenticated Access", 303
+            else:
+                payload = {
+                    'user': username,
+                    'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=30)
+                }
+                token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+                return jsonify({'Status': "Success", "Status Code": 200, "token": token, "username": username})
 
 
 @app.route('/logout', methods=['POST'])
+@tracing.trace()
 @token_required
 def logout():
-    token = request.headers['Authorization']
-    redis_command(redis_client.set, token, 'blacklisted', ex=30*60)  # Blacklist for 30 minutes
-    return jsonify({'message': 'Logged out successfully'})
+    with tracer.start_active_span('/auth/logout') as scope:
+        token = request.headers['Authorization']
+        redis_command(redis_client.set, token, 'blacklisted', ex=30*60)  # Blacklist for 30 minutes
+        return jsonify({'message': 'Logged out successfully'})
 
 @app.route('/clearData', methods=['POST'])
+@tracing.trace()
 def clearContacts():
     users_collection.delete_many({})
     return "All data cleared from contacts collection", 200
-# @app.route('/protected', methods=['GET'])
-# @token_required
-# def protected():
-#     return jsonify({'message': 'This is protected'})
 
 @app.route("/getUserInfos", methods=["GET"])
+@tracing.trace()
 def getUserInfos():
-    customer_datas = users_collection.find()
-    datas = []
-    for contact in customer_datas:
-        contact_dict = {key: value for key, value in contact.items() if key != '_id'}
-        datas.append(contact_dict)
-    return jsonify(datas)
+    with tracer.start_active_span('/auth/getUserInfos') as scope:
+        customer_datas = users_collection.find()
+        datas = []
+        for contact in customer_datas:
+            contact_dict = {key: value for key, value in contact.items() if key != '_id'}
+            datas.append(contact_dict)
+        return jsonify(datas)
 
 def makeCustomerInfo(jsonData):
     username = jsonData.get('username')
