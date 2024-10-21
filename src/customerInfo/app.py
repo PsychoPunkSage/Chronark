63874363af84cb2906cab26f7f17773bb3b2a9e9
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-21 15:46:23 +0530

    fix::> integrated jaeger

--- src/customerInfo/app.py
+++ src/customerInfo/app.py
@@ -2,13 +2,50 @@ import os
 from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
 SELF_PORT = os.environ.get('SELF_PORT')
 MONGO_DB_HOST = os.environ.get('MONGO_DB_HOST')
 MONGO_DB_PORT = os.environ.get('MONGO_DB_PORT')
 MONGO_DB_USERNAME = os.environ.get('MONGO_DB_USERNAME')
 MONGO_DB_PASSWORD = os.environ.get('MONGO_DB_PASSWORD')
+# Jaegar integration
+JAEGER_AGENT_HOST = os.environ.get('JAEGER_AGENT_HOST')
+JAEGER_AGENT_PORT = os.environ.get('JAEGER_AGENT_PORT')
+JAEGER_SERVICE_URL = 'http://' + JAEGER_AGENT_HOST + ':' + JAEGER_AGENT_PORT
 
 app = Flask(__name__)
+
+def init_tracer():
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'local_agent': {
+                'reporting_host': JAEGER_AGENT_HOST,
+                'reporting_port': JAEGER_AGENT_PORT,
+            },
+            'logging': True,
+        },
+        service_name='ms-cus-info',
+    )
+    return config.initialize_tracer()
+
+tracer = init_tracer()
+tracing = FlaskTracing(tracer, True, app)
+
+def trace_mongo_operation(scope, operation: str, collection_name: str, query: dict):
+    span = scope.span
+    
+    span.set_tag('db.type', 'mongodb')
+    span.set_tag('db.collection', collection_name)
+    span.set_tag('db.operation', operation)
+    span.log_kv({'query': query})
+
+# ================================================================================================ #
 
 db_client = MongoClient(
     username=MONGO_DB_USERNAME, 
@@ -48,64 +85,91 @@ class CustomerInfoTemplate:
         self.customer_pic_url = customer_pic_url # No need to add
 
 @app.route('/', methods=['GET'])
+@tracing.trace()
 def index():
-    return render_template(
-        'index.html',
-        m_client=db_client , 
-        MONGO_DB_HOST=MONGO_DB_HOST, 
-        MONGO_DB_PORT=MONGO_DB_PORT, 
-        MONGO_DB_USERNAME=MONGO_DB_USERNAME, 
-        MONGO_DB_PASSWORD=MONGO_DB_PASSWORD
-    )
+    with tracer.start_active_span('/cus-info/') as scope:
+        return render_template(
+            'index.html',
+            m_client=db_client , 
+            MONGO_DB_HOST=MONGO_DB_HOST, 
+            MONGO_DB_PORT=MONGO_DB_PORT, 
+            MONGO_DB_USERNAME=MONGO_DB_USERNAME, 
+            MONGO_DB_PASSWORD=MONGO_DB_PASSWORD
+        )
 
 # ===================================================================================================================================================================================== #
 
 @app.route("/getCustomerInfo/<string:username>", methods=["GET"])
+@tracing.trace()
 def getCustomerInfo(username):
-    customer_data = customer_collection.find_one({"username": username}, {"_id": 0})
-    if customer_data is None:
-        return jsonify({"message": "No customer found"}), 404
-    else:
-        return jsonify(customer_data)
+    with tracer.start_active_span(f'/cus-info/getCustomerInfo/{username}') as scope:
+        with tracer.start_active_span('mongo_insert') as mongo_span:
+            trace_mongo_operation(mongo_span, 'find', '/cus-info/getCustomerInfo', username)
+            customer_data = customer_collection.find_one({"username": username}, {"_id": 0})
+
+        if customer_data is None:
+            return jsonify({"message": "No customer found"}), 404
+        else:
+            return jsonify(customer_data)
 
 @app.route("/getCustomerInfos", methods=["GET"])
+@tracing.trace()
 def getCustomerInfos():
-    customer_datas = customer_collection.find()
-    datas = []
-    for contact in customer_datas:
-        contact_dict = {key: value for key, value in contact.items() if key != '_id'}
-        datas.append(contact_dict)
-    return jsonify(datas)
+    with tracer.start_active_span('/cus-info/getCustomerInfos') as scope:
+        with tracer.start_active_span('mongo_insert') as mongo_span:
+            trace_mongo_operation(mongo_span, 'find', '/cus-info/getCustomerInfos', {})
+            customer_datas = customer_collection.find()
+
+        datas = []
+        for contact in customer_datas:
+            contact_dict = {key: value for key, value in contact.items() if key != '_id'}
+            datas.append(contact_dict)
+        return jsonify(datas)
 
 @app.route("/updateCustomerInfo", methods=["POST", "PUT"])
+@tracing.trace()
 def updateCustomerInfo():
-    jsondata = request.json
-    username = jsondata.get('username')
-    existing_user = customer_collection.find_one({"username": username})
+    with tracer.start_active_span('/cus-info/updateCustomerInfo') as scope:
+        jsondata = request.json
+        username = jsondata.get('username')
 
-    if request.method == "POST":
-        if existing_user:
-            # Preserve the existing values for account_balance, dmat_balance, and account_number
-            jsondata['account_number'] = existing_user.get('account_number')
+        with tracer.start_active_span('mongo_insert') as mongo_span:
+            trace_mongo_operation(mongo_span, 'find', '/cus-info/updateCustomerInfo', username)
+            existing_user = customer_collection.find_one({"username": username})
 
-            customer_collection.update_one({"username": username}, {"$set": jsondata})
-        else:
-            customer_collection.insert_one(jsondata)
-        return "Success", 200
-    
-    if request.method == "PUT":
-        if existing_user:
-            customer_collection.update_one({"username": username}, {"$set": jsondata})
-        else:
-            return jsonify({"message": "No customer found"}), 404
-        return "Success", 200
+        if request.method == "POST":
+            if existing_user:
+                # Preserve the existing values for account_balance, dmat_balance, and account_number
+                jsondata['account_number'] = existing_user.get('account_number')
+
+                with tracer.start_active_span('mongo_insert') as mongo_span:
+                    trace_mongo_operation(mongo_span, 'update', '/cus-info/updateCustomerInfo', jsondata)
+                    customer_collection.update_one({"username": username}, {"$set": jsondata})
+            else:
+                with tracer.start_active_span('mongo_insert') as mongo_span:
+                    trace_mongo_operation(mongo_span, 'insert', '/cus-info/updateCustomerInfo', jsondata)
+                    customer_collection.insert_one(jsondata)
+            return "Success", 200
+
+        if request.method == "PUT":
+            if existing_user:
+                with tracer.start_active_span('mongo_insert') as mongo_span:
+                    trace_mongo_operation(mongo_span, 'update', '/cus-info/updateCustomerInfo', username)
+                    customer_collection.update_one({"username": username}, {"$set": jsondata})
+            else:
+                return jsonify({"message": "No customer found"}), 404
+            return "Success", 200
 
 # ===================================================================================================================================================================================== #
 
 @app.route('/clearData', methods=['POST'])
+@tracing.trace()
 def clearContacts():
-    customer_collection.delete_many({})
-    return "All data cleared from contacts collection", 200
+    with tracer.start_active_span('/cus-info/clearData') as scope:
+        with tracer.start_active_span('mongo_insert') as mongo_span:
+            trace_mongo_operation(mongo_span, 'delete', '/cus-info/clearContacts', {})
+            customer_collection.delete_many({})
+        return "All data cleared from contacts collection", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=SELF_PORT, debug=True)
