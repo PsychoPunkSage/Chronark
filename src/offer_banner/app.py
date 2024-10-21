aa9484a0f66ac199310e97db1a618c1beeec1a39
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-21 15:47:47 +0530

    fix::> integrated jaeger

--- src/offer_banner/app.py
+++ src/offer_banner/app.py
@@ -4,8 +4,6 @@ import time
 import redis
 from flask import Flask, jsonify, render_template, request
 
-# Jaeger tracing imports
-# from opentracing.ext import tags
 from jaeger_client import Config
 from flask_opentracing import FlaskTracing
 
@@ -22,25 +20,39 @@ JAEGER_AGENT_PORT = os.environ.get('JAEGER_AGENT_PORT')
 
 r_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
-# Jaeger configuration - start
-def initialize_tracer():
+def init_tracer():
     config = Config(
         config={
-            'sampler': {'type': 'const', 'param': 1},
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
             'local_agent': {
                 'reporting_host': JAEGER_AGENT_HOST,
                 'reporting_port': JAEGER_AGENT_PORT,
             },
             'logging': True,
         },
-        # ToCheck: offer-banner...
-        service_name="offer-banner",
+        service_name='ms-offer-banner',
     )
     return config.initialize_tracer()
 
-tracer = initialize_tracer()
+tracer = init_tracer()
 tracing = FlaskTracing(tracer, True, app)
-# Jaeger configuration - end
+
+def trace_redis_operation(scope, operation: str, key: str, value: str = None):
+    span = scope.span
+    
+    span.set_tag('cache.type', 'redis')
+    span.set_tag('cache.operation', operation)
+    span.set_tag('cache.key', key)
+    
+    if value:
+        span.set_tag('cache.value', value)  # Optionally log value for 'set' operations
+    
+    span.log_kv({'key': key, 'value': value if value else 'N/A'})
+
+# ================================================================================================ #
 
 def redis_command(command, *args):
     max_retries = 3
@@ -69,53 +81,69 @@ class Ad:
 @app.route('/', methods=['GET'])
 @tracing.trace()
 def index():
-    return render_template('index.html', r_client=r_client, REDIS_HOST=REDIS_HOST, REDIS_PORT=REDIS_PORT, REDIS_PASSWORD=REDIS_PASSWORD)
+    with tracer.start_active_span('/offer-banner/') as scope:
+        return render_template('index.html', r_client=r_client, REDIS_HOST=REDIS_HOST, REDIS_PORT=REDIS_PORT, REDIS_PASSWORD=REDIS_PASSWORD)
 
 
 @app.route('/getAds', methods=['GET'])
 @tracing.trace()
 def getAds():
-    ads = {}
-    adIDs = redis_command(r_client.scan_iter, "*")
-    for adID in adIDs:
-        ad = getAd(adID)
-        if "adID" in ad:
-            ads[ad["adID"]] = ad
-        else:
-            print(f"Invalid ad data for adID {adID}: {ad}")
-    return jsonify(ads)
+    with tracer.start_active_span('/offer-banner/getAds') as scope:
+        ads = {}
 
+        with tracer.start_active_span('redis_scan_iter') as redis_span:
+            trace_redis_operation(redis_span, 'scan_iter', '*')
+            adIDs = redis_command(r_client.scan_iter, "*")
+
+        for adID in adIDs:
+            ad = getAd(adID)
+            if "adID" in ad:
+                ads[ad["adID"]] = ad
+            else:
+                print(f"Invalid ad data for adID {adID}: {ad}")
+        return jsonify(ads) 
 
 @app.route('/getAd/<string:adID>', methods=['GET'])
 @tracing.trace()
 def getAd(adID):
-    ad = redis_command(r_client.get, adID)
-    if ad is None:
-        return {}, 404
-    else:
-        try:
-            return json.loads(ad.decode('utf-8'))
-        except json.JSONDecodeError as e:
-            print(f"JSONDecodeError: {e}")
-            print(f"Raw ad data: {ad}")
-            return {}
+    with tracer.start_active_span('/offer-banner/getAd/<adID>') as scope:
+
+        with tracer.start_active_span('redis_get') as redis_span:
+            trace_redis_operation(redis_span, 'get', adID)
+            ad = redis_command(r_client.get, adID)
+
+        if ad is None:
+            return {}, 404
+        else:
+            try:
+                return json.loads(ad.decode('utf-8'))
+            except json.JSONDecodeError as e:
+                print(f"JSONDecodeError: {e}")
+                print(f"Raw ad data: {ad}")
+                return {}
 
 @app.route('/updateAd', methods=['POST'])
 @tracing.trace()
 def updateAd():
-    jsonData = request.json
-    ad = Ad(**jsonData)
-    redis_command(r_client.set, ad.adID, json.dumps(ad.__dict__))
-    return "Success", 200
+    with tracer.start_active_span('/offer-banner/updateAd') as scope:
+        jsonData = request.json
+        ad = Ad(**jsonData)
+
+        with tracer.start_active_span('redis_set') as redis_span:
+            trace_redis_operation(redis_span, 'set', ad.adID, json.dumps(ad.__dict__))
+            redis_command(r_client.set, ad.adID, json.dumps(ad.__dict__))
+
+        return "Success", 200
 
 @app.route('/updateAds', methods=['POST'])
 @tracing.trace()
 def updateAds():
-    jsonData = request.json
-    for ad_data in jsonData:
-        ad = Ad(**ad_data)
-        updateAd(ad)
-    return "Success", 200
+    with tracer.start_active_span('/offer-banner/updateAds') as scope:
+        jsonData = request.json
+        for ad_data in jsonData:
+            ad = Ad(**ad_data)
+            updateAd(ad)
+        return "Success", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=SELF_PORT, debug=True)
