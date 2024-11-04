f301de60b123f5b1afa5ccfb0a5df0e9a5ce98f1
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-11-05 00:48:41 +0530

    fix::> Improved logging

--- scripts/load_test/load.sh
+++ scripts/load_test/load.sh
@@ -28,18 +28,20 @@ cleanup() {
       for url in "$login_data" "$customerInfo_data"; do
         response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$url" -H "Content-Type: application/json" -d "{\"username\": \"$username\"}")
         if [ "$response" -eq 200 ]; then
-          echo "Data for username '$username' cleared successfully at $url."
+          echo "[CLEANUP][+]  Data for username '$username' cleared successfully at $url."
         elif [ "$response" -eq 404 ]; then
-          echo "No data found for username '$username' at $url."
+          echo "[CLEANUP][?]  No data found for username '$username' at $url."
         else
-          echo "Failed to clear data for username '$username' at $url. Status code: $response"
+          echo "[CLEANUP][-]  Failed to clear data for username '$username' at $url. Status code: $response"
         fi
       done
     )&
   done
 
   wait
+  echo "================================================================"
   echo "Cleanup completed for users from $start_index to $((end_index - 1))."
+  echo "================================================================"
 }
 
 login_user() {
@@ -47,14 +49,12 @@ login_user() {
   password=$2
   cookie_file="cookie/cookies_$username.txt"
 
-  echo "username: $username || password: $password"
-
   login_response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$login_link" \
     -d "username=$username&password=$password" --cookie-jar "$cookie_file")
   if [ "$login_response" -eq 200 ] || [ "$login_response" -eq 302 ]; then
-    echo "User $username logged IN successfully. Cookies saved in $cookie_file."
+    echo "[LOGIN][+]     User $username logged IN successfully. Cookies saved in $cookie_file."
   else
-    echo "Failed to log-in user $username. Status code: $login_response"
+    echo "[LOGIN][-]     Failed to log-in user $username. Status code: $login_response"
   fi
 }
 
@@ -64,9 +64,9 @@ logout_user() {
   logout_response=$(curl -s -o /dev/null -w "%{http_code}" -L "$logout_link" \
     --cookie "$cookie_file" --cookie-jar "$cookie_file")
   if [ "$logout_response" -eq 200 ]; then
-    echo "User $username logged OUT successfully. Cookies saved in $cookie_file."
+    echo "[LOGOUT][+]   User $username logged OUT successfully. Cookies saved in $cookie_file."
   else
-    echo "Failed to log-out user $username. Status code: $logout_response"
+    echo "[LOGOUT][-]   Failed to log-out user $username. Status code: $logout_response"
   fi
 }
 
@@ -112,19 +112,21 @@ add_users() {
     (
       register_response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$register_url" -H "Content-Type: application/json" -d "$user_data")
       if [ "$register_response" -eq 200 ]; then
-        echo "User $username registered successfully."
+        echo "[REGISTER][+]  User $username registered successfully."
         
         # Login user (after registration) in the background
         login_user "$username" "$password"
       else
-        echo "Failed to register user $username. Status code: $register_response"
+        echo "[REGISTER][-]  Failed to register user $username. Status code: $register_response"
       fi
     ) &
   done
 
   # Wait for all background processes to complete
   wait
+  echo "================================================"
   echo "All users registered and logged in concurrently."
+  echo "================================================"
 }
 
 # Main script to handle flags
